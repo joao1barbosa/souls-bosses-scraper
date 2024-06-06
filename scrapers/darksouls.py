f8d9bfcc165adcca913f38c0a09151e026f89ecf
joao1barbosa <joao.rbarbosa3@gmail.com>
2024-06-06 20:47:43 -0300

    Add Scraping for dlc bosses and refactor the code

--- scrapers/darksouls.py
+++ scrapers/darksouls.py
@@ -1,35 +1,41 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import csv
+import os.path
 
-url = 'https://darksouls.wiki.fextralife.com/Bosses'
+boss_data = []
+csv_file = 'bosses.csv'
+file_exists = os.path.isfile(csv_file)
 
+def scraping(url, dlc):
+    driver = webdriver.Chrome()  
+    driver.get(url)
 
-webdriver = webdriver.Chrome()
-webdriver.get(url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
 
-webdriver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(3)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    driver.quit()
 
-page_source = webdriver.page_source
-soup = BeautifulSoup(page_source, 'lxml')
-webdriver.quit()
+    boss_cards = soup.find_all('h3', class_='col-sm-4')
 
-boss_names = []
+    for boss in boss_cards:
+        boss_name = boss.text.strip()
+        is_optional = boss_name.endswith('*')
+        boss_name = boss_name.rstrip('*').strip()
+        boss_data.append([boss_name,'ds1', None, dlc, is_optional])
+ 
+url_base = 'https://darksouls.wiki.fextralife.com/'
 
-boss_cards = soup.find_all('h3', class_='col-sm-4')
+scraping(url_base + "Bosses", False)
+scraping(url_base + "Expansion+Bosses", True)
 
-for boss in boss_cards:
-    boss_names.append(boss.text.strip())
+with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    if not file_exists:
+        writer.writerow(['name', 'game','picture', 'dlc', 'optional'])
+    writer.writerows(boss_data)
 
-print(boss_names)
-
-# a_tags = soup.find_all('a', class_='wiki_link')
-
-# # Append the text content of each tag to the list
-# for tag in a_tags:
-#     boss_names.append(tag.text.strip())
-
-# # Print the collected names
-# for boss_name in boss_names:
-#     print(boss_name)
+print(f"Data has been appended to {csv_file}")
